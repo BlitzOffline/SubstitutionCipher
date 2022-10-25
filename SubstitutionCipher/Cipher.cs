1131375048246234bd9f0e90ab605d974cc6ae06
BlitzOffline <52609756+BlitzOffline@users.noreply.github.com>
2022-10-25 21:12:06 +0300

    Added comments

--- SubstitutionCipher/Cipher.cs
+++ SubstitutionCipher/Cipher.cs
@@ -3,9 +3,18 @@ using System.Linq;
 
 namespace SubstitutionCipher
 {
+    /**
+     * A simple implementation of the Caesar Cipher.
+     */
     public class Cipher
     {
+        /**
+         * Determines the amount that the message will be shifted by the cipher.
+         */
         public byte ShiftAmount { get; set; }
+        /**
+         * Determines whether the message will be shifted to the left or to the right.
+         */
         public bool LeftShift { get; set; } = true;
 
         public Cipher(byte shiftAmount)
